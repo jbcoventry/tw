f60976b1901c34df00f262ddc105b05344642490
Jayden <contact@jbcov.com>
2023-05-10 21:48:43 +1000

    api integration test complete

--- src/index.js
+++ src/index.js
@@ -12,8 +12,12 @@ export default {
       return new Response("The request was not a GET or a POST");
     }
     async function handlePOSTRequest() {
-      const reqBody = JSON.stringify(await request.json());
-      await kv.put("myKey", reqBody);
+      const reqBody = await request.json();
+      const wineryNumber = reqBody.wineryNumber;
+      console.log(wineryNumber);
+      const data = await getDataFromAPI(wineryNumber);
+
+      await kv.put("myKey", JSON.stringify(data));
 
       return new Response(reqBody, {
         headers: {
@@ -32,5 +36,29 @@ export default {
         },
       });
     }
+    async function getDataFromAPI(selectedWinery) {
+      const response = await fetch(
+        `https://api.apify.com/v2/datasets/XHxNIww6Ofj811blJ/items?offset=${selectedWinery}&limit=1`,
+        {
+          method: "GET",
+          headers: {
+            "content-type": "application/json;charset=UTF-8",
+            Authorization: `bearer ${env.ApifyToken}`,
+          },
+        }
+      );
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      const data = await response.json();
+      console.log(data[0]);
+      return data[0];
+    }
+  },
+  async scheduled(event, env, ctx) {
+    const kv = env.kv;
+    const data = Number(await kv.get("countKey"));
+    const newData = data + 1;
+    await kv.put("countKey", newData);
   },
 };
